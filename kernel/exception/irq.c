734787259ee62ee03a088d3b580e176615c4a248
ubuntu <ubuntu@fool.com>
2022-04-03 18:24:12 +0800

    finished part A.

--- kernel/exception/irq.c
+++ kernel/exception/irq.c
@@ -40,6 +40,10 @@ void handle_irq(int type)
 	 *	The irq is not from the kernel
 	 * 	The thread being interrupted is an idle thread.
 	 */
+	if (type == IRQ_EL0_64 || type == IRQ_EL0_32 ||
+		current_thread->thread_ctx->type == TYPE_IDLE) {
+		lock_kernel();
+	}
 
 	plat_handle_irq();
 
--- kernel/main.c
+++ kernel/main.c
@@ -67,6 +67,8 @@ void main(void *addr)
 	kernel_lock_init();
 	kinfo("[ChCore] lock init finished\n");
 
+	lock_kernel();
+
 	/* Init scheduler with specified policy. */
 	sched_init(&rr);
 	kinfo("[ChCore] sched init finished\n");
@@ -112,6 +114,7 @@ void secondary_start(void)
 	 * Inform the BSP at last to start cpu one by one
 	 * Hints: use cpu_status
 	*/
+	cpu_status[smp_get_cpu_id()] = cpu_run;
 
 #ifndef TEST
 	run_test(false);
@@ -121,6 +124,7 @@ void secondary_start(void)
 	 *  Lab4
 	 *  Acquire the big kernel lock
 	 */
+	lock_kernel();
 
 	/* Where the AP first returns to the user mode */
 	sched();
